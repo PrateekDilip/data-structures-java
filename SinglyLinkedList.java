d8fdb98654313cb0422847c7d57ac1375f4e03df
Prateek Dilip <halwe.prateek@gmail.com>
2017-07-24 15:44:38 +0530

    node/linklist class defined

---
+++ SinglyLinkedList.java
@@ -0,0 +1,47 @@
+class node {
+    protected int data; 
+    protected node link;
+    public node() {
+        data = -1;
+        link = null;
+    }
+    public node(int d, node l) {
+        data = d;
+        link = l;
+    }
+    public void setLink(node n) {
+        link = n;
+    }
+    public void setData(int d) {
+        data = d;
+    }
+    public node getLink() {
+        return link;
+    }
+    public int getData() {
+        return data;
+    }
+}
+class linkedlist {
+    protected node start;
+    protected node end;
+    public int size;
+    public linkedlist() {
+        start = null;
+        end = null;
+        size = 0;
+    } 
+    public isEmpty() {
+        return start == null;
+    }
+    public getSize() {
+        return size;
+    }
+}
+
+
+public class SinglyLinkedList {
+    public static void main (String args[]) {
+        
+    }
+}
