53e12df3e9952242ef2ad69c880b72d484e38ac7
Prateek Dilip <halwe.prateek@gmail.com>
2017-07-25 11:37:48 +0530

    added sll code in to a separate folder

---
+++ SLL/SinglyLinkedList.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+class node {
+    protected int data; 
+    protected node link;
+    public node() {
+        data = -1;
+        link = null;
+    }
+    public node(int d, node l) {
+        data = d;
+        link = l;
+    }
+    public void setLink(node n) {
+        link = n;
+    }
+    public void setData(int d) {
+        data = d;
+    }
+    public node getLink() {
+        return link;
+    }
+    public int getData() {
+        return data;
+    }
+}
+class linkedlist {
+    protected node start;
+    protected node end;
+    public int size;
+    public linkedlist() {
+        start = null;
+        end = null;
+        size = 0;
+    } 
+    public boolean isEmpty() {
+        return start == null;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void insertAtEnd(int val) {
+        node temp = new node(val, null);
+        size++;
+        if(start == null) {
+            start = temp;
+            end = start;
+        } else {
+            end.setLink(temp);
+            end = temp;
+        }
+    }
+    public void display() {
+        if (start.getLink() == null) { 
+            System.out.print(start.getData()+ "->");
+            return;
+        }
+        if (size == 0) {
+            System.out.println("List is empty\n");
+            return;
+        }
+        node ptr = start;
+        while (ptr.getLink() != null) {
+            System.out.print(ptr.getData()+ "->");
+            ptr = ptr.getLink();
+        }
+            System.out.print(ptr.getData()+ "\n");
+    }
+}
+
+
+public class SinglyLinkedList {
+    public static void main (String args[]) {
+        Scanner scan = new Scanner(System.in);
+        linkedlist list = new linkedlist();
+        list.insertAtEnd(scan.nextInt());         
+        list.insertAtEnd(scan.nextInt());         
+        list.insertAtEnd(scan.nextInt());         
+        list.insertAtEnd(scan.nextInt());         
+        list.display();
+    }
+}
