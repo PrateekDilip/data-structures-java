b38431b9d5f1ed742a217e8990a983f385bf256d
Prateek Dilip <halwe.prateek@gmail.com>
2017-07-27 15:55:21 +0530

    add queue implementatio and function skeletons

---
+++ queue/queueImplement.java
@@ -0,0 +1,30 @@
+public class arrayQueue {
+    protected int Queue[];
+    protected int rear, front, len, size;
+    
+    public arrayQueue(int N) {
+   
+    }
+    public boolean isEmpty() {
+   
+    }
+    public boolean isFull() {
+    
+    }
+    public int getSize() {
+    }
+    public void enqueue(int item) {
+    
+    }
+    public int dequeue() {
+    
+    }
+    public void display() {
+    
+    }
+}
+public class queueImplement {
+    public static void main(String args[]) {
+        
+    }
+}
